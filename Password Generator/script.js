ee20771047d34432f1568a7be31f629152844d25
Surya <beastsurya47@gmail.com>
2024-01-11 18:10:05 +0530

    Added strong password

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -5,11 +5,21 @@ inputBox.value = ''
 inputBox.disabled = true
 const map = new Map
 
-function gen(){
+function generatePassword(){
+    const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const lower = 'abcdefghijklmnopqrstuvwxyz'
+    const number = '1234567890'
+    const symbols = '!@#$%^&*()_+=-<>?/{}[]'
+    const char = upper+lower+number+symbols;
     let res = ''
-    const char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_{}[]><?'
+    ;
+    res += getRandomChar(lower)
+    res += getRandomChar(number)
+    res += getRandomChar(upper)
+    res += getRandomChar(symbols)
     
-    for (let i = 0; i < 8; i++) {
+    
+    for (let i = 0; i < 4; i++) {
         const index = Math.floor(Math.random()*char.length)
         res += char.charAt(index)
     }
@@ -24,6 +34,13 @@ function gen(){
         },2000)
         console.log('Copy button clicked');
     })
+
+    function getRandomChar(charset) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        return charset.charAt(randomIndex);
+      }
 }
 
-genBtn.addEventListener('click',gen)
+
+
+genBtn.addEventListener('click',generatePassword)
